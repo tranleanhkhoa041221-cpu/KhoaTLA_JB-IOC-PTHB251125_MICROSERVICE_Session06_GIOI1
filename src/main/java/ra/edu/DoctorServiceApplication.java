884d073844094afeece1724530bac11c5e8ca4b2
Trần Lê Anh Khoa <tranleanhkhoa041221@gmail.com>
2026-06-22 20:34:02 +0700

    Upload btvn microservice session06 bai3 gioi1

--- src/main/java/ra/edu/DoctorServiceApplication.java
+++ src/main/java/ra/edu/DoctorServiceApplication.java
@@ -2,8 +2,10 @@ package ra.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DoctorServiceApplication {
 
     public static void main(String[] args) {
